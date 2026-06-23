e4f42ce415fd3cadcf146915d2c47dd36e276b47
jagoblast <blastjago@gmail.com>
2026-06-23 19:41:43 +0700

    Update _middleware.ts

--- app/routes/admin/_middleware.ts
+++ app/routes/admin/_middleware.ts
@@ -1,12 +1,12 @@
-import { createMiddleware } from 'hono/factory'
+import { createRoute } from 'honox/factory'
 import { getCookie } from 'hono/cookie'
 import { verify } from 'hono/jwt'
 
-export default createMiddleware(async (c, next) => {
+export default createRoute(async (c, next) => {
   // Biarkan halaman login bisa diakses tanpa token
   if (c.req.path === '/admin/login') {
     await next()
-    return // Berhenti di sini, biarkan route login yang memproses Response
+    return
   }
   
   const token = getCookie(c, 'auth_token')
